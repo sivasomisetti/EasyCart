46009e1a0178a91961b43a362fb531427fc2c072
ajay <ajay@ajays-MacBook-Air.local>
2025-09-21 23:28:57 +0530

    Final fixes for inngest setup git push origin main

--- app/api/route.js
+++ app/api/route.js
@@ -1,12 +1,24 @@
+// import { serve } from "inngest/next";
+// import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+
+// // Create an API that serves zero functions
+// export const { GET, POST, PUT } = serve({
+//   client: inngest,
+//   functions: [
+//     syncUserCreation,
+//     syncUserUpdation,
+//     syncUserDeletion
+//   ],
+// });
+
+// src/app/api/inngest/route.js
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
+import { inngest, syncUserCreation } from "../../../config/inggest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    syncUserCreation,
-    syncUserUpdation,
-    syncUserDeletion
+    syncUserCreation // Your function is imported from inggest.js
   ],
 });
