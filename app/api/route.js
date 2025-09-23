a81eff17f46494387a748b0832f0efe6a4bb200f
Somisetti Nagasiva <sivasomisetti1432@gmail.com>
2025-09-23 20:36:12 +0530

    fix: correct module path for inngest import

--- app/api/route.js
+++ app/api/route.js
@@ -1,7 +1,6 @@
 
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserUpdation, syncUserDeletion } from "../../../config/inngest"; 
-// make sure file name is `inngest.js` (two n's) and the path is correct relative to this file
+import { inngest, syncUserCreation, syncUserUpdation, syncUserDeletion } from "../../config/inngest";// make sure file name is `inngest.js` (two n's) and the path is correct relative to this file
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
